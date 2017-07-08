4a385ca8ca22f53df22974d9ca1f388ddf53eac9
Andrew Suprun <andrew.suprun@gmail.com>
2017-07-08 17:23:01 -0400

    dummy swift project

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "friendly-fire",
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "friendly-fire",
+            dependencies: []),
+    ]
+)
