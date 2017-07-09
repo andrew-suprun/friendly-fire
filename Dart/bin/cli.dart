8f3a3fe4580f5560138651699f5d15832f92b42e
Andrew Suprun <andrew.suprun@gmail.com>
2017-07-09 19:44:18 -0400

    Dart: `await for` instead of `listen()`

--- Dart/bin/cli.dart
+++ Dart/bin/cli.dart
@@ -3,9 +3,9 @@ import 'dart:convert';
 
 main() async {
   var process = await Process.start('dart', ['bin/friendly-fire.dart']);
-  process.stdout
-      .transform(UTF8.decoder)
-      .listen((data) { print(data); });
+  await for (final data in process.stdout.transform(UTF8.decoder)) {
+    print(data);
+  }
   process.exitCode.then((exitCode) {
     print('exit code: $exitCode');
   });
